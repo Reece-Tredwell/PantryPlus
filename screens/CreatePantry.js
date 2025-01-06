e9ad00b1b8b8a9b54f7fd66478cda4256b5698c0
Reece <r.tredwell@outlook.com>
2025-01-06 11:18:36 +1100

    New Screen to create pantry and login, add pr9o0duct page is done and saves to DB, need to create the linl between actually loggin in and the information pulled after the login button is pressed

--- screens/CreatePantry.js
+++ screens/CreatePantry.js
@@ -1,38 +1,203 @@
-import { View, Text, StyleSheet, ViewBase, Button, TouchableOpacity } from 'react-native';
-import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Modal, TouchableOpacity } from 'react-native';
+import config from 'D:\\PersonalProjects\\PantryPlus\\config.json';
+import React, { useState, useEffect, } from 'react';
+import { DataTable, TextInput } from 'react-native-paper';
 
 export default function CreatePantry() {
+  const [CreateModalVisible, setCreateModalVisible] = useState(false);
+  const [LoginModalVisible, setLoginModalVisible] = useState(false);
+  const [PantryUserName, UsernameSetText] = useState('');
+  const [Password, PasswordSetText] = useState('');
 
-    const CreateNewPantryTable = () =>{
-        alert("New Table being made")
+  const openCreateModal = () => {
+    setCreateModalVisible(true);
+  }
+
+  const openLoginModal = () => {
+    setLoginModalVisible(true);
+  }
+
+  const CreateNewPantryTable = async (username, password) => {
+    console.log("Creating")
+    const response = await fetch("https://cfaem0qp2j.execute-api.ap-southeast-2.amazonaws.com/Production/CreateNewPantryTable", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": config["PantryCreateAPIKey"]
+      },
+      body: JSON.stringify({
+        "username": username,
+        "password": password
+      })
+    })
+    console.log(response)
+    console.log(config["PantryCreateAPIKey"])
+    if (!response.ok) {
+      console.log("Failed")
+      console.log(response)
+      throw new Error(`Failed to create pantry table: ${response.status} ${response.statusText}`);
     }
+    const data = await response.json();
+    console.log(data);
+  };
 
 
-return(
-    <View style={styles.buttonView}>
-        <TouchableOpacity style={styles.CreatePantryButton} onPress={CreateNewPantryTable}>
-            <Text style={styles.Text}> Create New Pantry!</Text>
+  const LoginToPantry = async (username, password) => {
+    console.log("Logging In")
+    const response = await fetch("https://cfaem0qp2j.execute-api.ap-southeast-2.amazonaws.com/Production/LoginToPantry", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": config["PantryCreateAPIKey"]
+      },
+      body: JSON.stringify({
+        "username": username,
+        "password": password
+      })
+    });
+
+    if (!response.ok) {
+      console.log("Failed")
+      console.log(response)
+    }else{
+      const data = await response.json();
+      console.log(data);
+    }
+  };
+
+
+    return (
+      <View style={styles.buttonView}>
+        <TouchableOpacity style={styles.CreatePantryButton} onPress={openCreateModal}>
+          <Text style={styles.Text}> Create New Pantry!</Text>
         </TouchableOpacity>
-    </View>
-    );
-}
 
-const styles = StyleSheet.create({
+
+        <TouchableOpacity style={styles.CreatePantryButton} onPress={openLoginModal}>
+          <Text style={styles.Text}> Sign Into a Pantry</Text>
+        </TouchableOpacity>
+
+        {CreateModalVisible && (
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={CreateModalVisible}
+            onRequestClose={() => setCreateModalVisible(false)}
+          >
+            <View style={styles.modalOverlay}>
+              <View style={styles.modalContent}>
+
+                <DataTable style={styles.container}>
+                  <DataTable.Row>
+                    <TextInput style={styles.TextInput} placeholder='Pantry Name' value={PantryUserName} onChangeText={UsernameSetText}></TextInput>
+                  </DataTable.Row>
+                  <DataTable.Row>
+                    <TextInput style={styles.TextInput} placeholder='Password' value={Password} onChangeText={PasswordSetText}></TextInput>
+                  </DataTable.Row>
+                </DataTable>
+                <View style={styles.ButtonRow}>
+                  <TouchableOpacity style={styles.CreateButton} onPress={() => setCreateModalVisible(false)}>
+                    <Text style={styles.closeButtonText}> Close </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.closeButton} onPress={() => CreateNewPantryTable(PantryUserName, Password)}>
+                    <Text style={styles.closeButtonText}>Create</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </Modal>
+        )}
+
+        {LoginModalVisible && (
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={LoginModalVisible}
+            onRequestClose={() => setLoginModalVisible(false)}
+          >
+            <View style={styles.modalOverlay}>
+              <View style={styles.modalContent}>
+
+                <DataTable style={styles.container}>
+                  <DataTable.Row>
+                    <TextInput style={styles.TextInput} placeholder='Pantry Name' value={PantryUserName} onChangeText={UsernameSetText}></TextInput>
+                  </DataTable.Row>
+                  <DataTable.Row>
+                    <TextInput style={styles.TextInput} placeholder='Password' value={Password} onChangeText={PasswordSetText}></TextInput>
+                  </DataTable.Row>
+                </DataTable>
+                <View style={styles.ButtonRow}>
+                  <TouchableOpacity style={styles.CreateButton} onPress={() => setLoginModalVisible(false)}>
+                    <Text style={styles.closeButtonText}> Close </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.closeButton} onPress={() => LoginToPantry(PantryUserName, Password)}>
+                    <Text style={styles.closeButtonText}>Login</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </Modal>
+        )}
+      </View>
+    );
+  }
+  const styles = StyleSheet.create({
+    TextInput: {
+      opacity: 10,
+      flex: 1,
+    },
+    ButtonRow: {
+      top: 10,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
     buttonView: {
-        top: '30%',
-        borderRadius: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
+      top: '30%',
+      borderRadius: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
     },
     CreatePantryButton: {
-        marginTop: 10,
-        padding: 10,
-        backgroundColor: '#2196F3',
-        borderRadius: 5,
+      marginTop: 10,
+      padding: 10,
+      backgroundColor: '#2196F3',
+      borderRadius: 5,
     },
-    Text:{
-        fontSize: 18,
-        marginBottom: 20,
+    Text: {
+      fontSize: 18,
+      marginBottom: 20,
+    },
+    closeButton: {
+      marginTop: 10,
+      padding: 10,
+      backgroundColor: '#2196F3',
+      borderRadius: 5,
+      left: 10
+    },
+    CreateButton: {
+      marginTop: 10,
+      padding: 10,
+      backgroundColor: '#2196F3',
+      borderRadius: 5,
+      right: 10
+    },
+    closeButtonText: {
+      color: 'white',
+      fontSize: 16,
+    },
+    modalOverlay: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+      width: 300,
+      height: 500,
+      padding: 20,
+      backgroundColor: 'white',
+      borderRadius: 10,
+      alignItems: 'center',
     },
 
-});
+  });
---
+++ scripts/CreateNewPantryTable/lambda_function.py
@@ -0,0 +1,46 @@
+import sys
+import psycopg2
+import boto3
+import json
+import os
+
+
+
+try:
+    # connection = psycopg2.connect(host = os.environ["host"],
+    #                                 user= os.environ["username"], 
+    #                                 password = os.environ["password"], 
+    #                                 dbname = os.environ["dbname"], 
+    #                                 port = os.environ["port"])
+    with open('D:\\PersonalProjects\\PantryPlus\\config.json', 'r') as file:
+        data = json.load(file)
+    connection = psycopg2.connect(host = data["host"],
+                                    user= data["username"], 
+                                    password = data["password"], 
+                                    dbname = data["dbname"], 
+                                    port = data["port"])
+except psycopg2.Error as e:
+    print(f"Connection Attempt Failed, error: {e}")
+    
+
+def getDBID():
+    s3 = boto3.client('s3')
+    DBID = s3.get_object(Bucket="pantrys", Key="CurrentPantryID.json")
+    # DBID = json.loads(DBID['Body'].read())
+    print(DBID)
+    #this is for Getting the PantryID
+    
+
+def lambda_handler(event, context):
+    getDBID()
+    CreateTable = f"""CREATE TABLE pantrys.{getDBID()}(
+        InsertID VARCHAR(255),
+        ProductID VARCHAR(255),
+        UserID VARCHAR(255),
+        DATEADDED VARCHAR(255)
+    )"""
+    with connection.cursor() as cur:
+        cur.execute(CreateTable)
+        connection.commit()
+
+lambda_handler(None, None)
